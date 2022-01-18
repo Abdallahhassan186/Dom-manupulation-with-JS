7c82d38e258dc1251ce91762ca96d5f8405ddec9
Abdallah Hassan <abdallahassan.ahs@gmail.com>
2022-01-19 09:36:44 +1100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,116 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+let sections = document.querySelectorAll('section');
+let navBar= document.querySelector('#navBar__list');
+
+
+
+
+//the below property gets the position of the top line of the page view relative to the page
+/**
+ * End Global Variables
+ * 
+ * Start Helper Functions
+*/
+function addSec(numb){
+    let tagContent=sections[0].innerHTML;
+    const newSec=document.createElement('section');
+    sections[0].parentElement.appendChild(newSec)
+    newSec.innerHTML=tagContent;
+    newSec.id="section"+numb;
+    newSec.dataset.nav="Section "+numb;
+    let newtagChild=newSec.firstElementChild;
+    newtagChild.firstElementChild.textContent='Section '+numb;
+   
+}
+addSec(4);
+addSec(5);
+function addScrollButton(){
+    let currentView= document.body.scrollTop;
+    if (currentView>300){
+        let scrollBut=document.createElement('button');
+        scrollBut.textContent='^';
+        scrollBut.style.cssText="background-color:transparent;border:5px;position: fixed;top: 550;z-index:5;font-size: 3em;";
+        let mainBody=document.querySelector('main');
+        mainBody.appendChild(scrollBut);
+    }
+}
+
+/**
+ * End Helper Functions
+ * 
+ * Begin Main Functions
+*/
+
+// build the nav
+function addListItems(){
+    let sections = document.querySelectorAll('section');
+    let myNavBar= document.querySelector('#navBar__list');
+    for (section of sections){
+        let listElem= document.createElement('li');
+        let listElemAnch=document.createElement('a');
+        listElemAnch.setAttribute('href','#'+section.id);
+        listElemAnch.textContent= section.dataset.nav;
+        listElem.appendChild(listElemAnch);
+        myNavBar.appendChild(listElem);
+    }
+}
+
+// Add class 'active' to section when near top of viewport
+function addHighlight(){
+    let sections = document.querySelectorAll('section');
+    sections.forEach(section =>{
+         const sectionTop = section.getBoundingClientRect().top;
+         if(sectionTop > 0 && sectionTop <= 450){
+          section.classList.add("your-active-class");
+        }else{
+            section.classList.remove("your-active-class");
+       }
+    })
+}
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * 
+ * Begin Events
+*/
+
+// Build menu 
+document.addEventListener('DOMContentLoaded',addListItems());
+// Scroll to section on link click
+navBar.addEventListener('click',function scrollToSec(e){
+    e.preventDefault();
+    const anch=e.target;
+    if (anch.tagName === 'A'){
+        let targetSecID= anch.getAttribute('href');
+        let targetSec=document.querySelector(targetSecID);
+        targetSec.scrollIntoView({behavior:'smooth'});};
+});
+// Set sections as active
+window.addEventListener("scroll", addHighlight);
+//window.addEventListener("scroll", addScrollButton);
