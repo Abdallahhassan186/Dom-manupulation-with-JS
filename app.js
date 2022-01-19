209e0002eeccbd1bc48ab2dc919c72d3f096eb5b
Abdallah Hassan <abdallahassan.ahs@gmail.com>
2022-01-20 07:48:59 +1100

    Update app.js

--- app.js
+++ app.js
@@ -24,16 +24,18 @@
 */
 let sections = document.querySelectorAll('section');
 let navBar= document.querySelector('#navBar__list');
+let scrollButton=document.querySelector('#scrollTop');
+let mainBody=document.querySelector('main');
 
 
 
 
-//the below property gets the position of the top line of the page view relative to the page
 /**
  * End Global Variables
  * 
  * Start Helper Functions
 */
+//add a new section to the page
 function addSec(numb){
     let tagContent=sections[0].innerHTML;
     const newSec=document.createElement('section');
@@ -47,15 +49,30 @@ function addSec(numb){
 }
 addSec(4);
 addSec(5);
+//Create the scroll to top button
+function myButton() {
+
+    let scrollBut=document.createElement('button');
+    scrollBut.id="scrollTop"
+    scrollBut.textContent='^';
+    scrollBut.style.cssText="display: none;background-color:transparent;border:5px;position: fixed;top: 550;z-index:5;font-size: 3em;color: white;";
+    let mainBody=document.querySelector('main');
+    mainBody.appendChild(scrollBut);
+}
+myButton();
+
+//Show a scroll button after scrolling below the page fold
 function addScrollButton(){
+    let scrollButton=document.querySelector('#scrollTop');
+
+    //the below property gets the position of the top line of the page view relative to the page
     let currentView= document.body.scrollTop;
-    if (currentView>300){
-        let scrollBut=document.createElement('button');
-        scrollBut.textContent='^';
-        scrollBut.style.cssText="background-color:transparent;border:5px;position: fixed;top: 550;z-index:5;font-size: 3em;";
-        let mainBody=document.querySelector('main');
-        mainBody.appendChild(scrollBut);
-    }
+
+    if (currentView > 300) {
+        scrollButton.style.display = "block";
+      } else {
+        scrollButton.style.display = "none";
+      }
 }
 
 /**
@@ -89,10 +106,10 @@ function addHighlight(){
           for (item of myNavBar){
               
               if (item.firstElementChild.getAttribute('href')==='#'+section.id){
-                  item.style.backgroundColor = "blue";
+            item.style.cssText = "background: rgb(0, 0, 0);background: linear-gradient(0deg, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0) 100%);";
               }
               else{
-                item.style.backgroundColor = "white";
+                item.style.cssText = "none";
               }
           }
         }else{
@@ -116,11 +133,20 @@ document.addEventListener('DOMContentLoaded',addListItems());
 navBar.addEventListener('click',function scrollToSec(e){
     e.preventDefault();
     const anch=e.target;
-    if (anch.tagName === 'A'){
+    if (anch.tagName === 'A'){// scrolls to the section of ID similar to the anchor's Href
         let targetSecID= anch.getAttribute('href');
         let targetSec=document.querySelector(targetSecID);
         targetSec.scrollIntoView({behavior:'smooth'});};
 });
 // Set sections as active
 window.addEventListener("scroll", addHighlight);
-//window.addEventListener("scroll", addScrollButton);
+
+//first event to show the scroll button
+window.addEventListener("scroll", addScrollButton);
+
+//second event to give it the ability to scroll
+mainBody.addEventListener('click',function scrollToTop(e){
+    let scrollButton=document.querySelector('#scrollTop');
+    if(e.target===scrollButton){
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}});
